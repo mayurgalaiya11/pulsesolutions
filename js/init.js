e0c312d142068541a9a921fcfbbc50babaeea9a8
mayurgalaiya11 <31320025+mayurgalaiya11@users.noreply.github.com>
2021-04-21 23:03:34 +0530

    new

---
+++ js/init.js
@@ -0,0 +1,25 @@
+/**
+ * Created by User on 23-10-2018.
+ */
+(function ($) {
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            dots:false,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        })
+
+    });
+})(jQuery);
